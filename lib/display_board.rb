2f412b01c2f127f37273605bdd9cad3778ad2771
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-06-20 00:00:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,9 @@
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-
+  
 def display_board
  puts "A Tic Tac Toe Board"
 end
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
